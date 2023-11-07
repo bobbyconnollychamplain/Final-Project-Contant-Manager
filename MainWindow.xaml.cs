89ff3fc85a03f596a3053f0767d2550ff8f1424f
Bobby Connolly <bobbyconnollychamplain@gmail.com>
2023-11-07 08:40:33 -0500

    Adding message boxes hello bye

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -24,5 +24,12 @@ namespace Final_Project_Contant_Manager
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Hello");
+
+            MessageBox.Show("Bye");
+        }
     }
 }
